62852be96af46d6740cbfe1dc1b4da02a7e80f33
ErlandasL <erlandaslisauskas@gmail.com>
2024-07-18 02:16:09 +0300

    Add: new feature for flights having 1 connection

--- FlightScraper/Models/Flight.cs
+++ FlightScraper/Models/Flight.cs
@@ -1,69 +1,96 @@
-namespace FlightScraper.Models;
-
-public class FlightModel
+namespace FlightScraper.Models
 {
-    public Header Header { get; set; }
-    public Body Body { get; set; }
-}
+    public class FlightModel
+    {
+        public Header Header { get; set; }
+        public Body Body { get; set; }
+    }
 
-public class Header
-{
-    public string Message { get; set; }
-    public int Code { get; set; }
-    public bool Error { get; set; }
-}
+    public class Header
+    {
+        public string Message { get; set; }
+        public int Code { get; set; }
+        public bool Error { get; set; }
+    }
 
-public class Body
-{
-    public Data Data { get; set; }
-}
+    public class Body
+    {
+        public Data Data { get; set; }
+    }
 
-public class Data
-{
-    public List<Journey> Journeys { get; set; }
-    public List<TotalAvailability> TotalAvailabilities { get; set; }
-}
+    public class Data
+    {
+        public List<Journey> Journeys { get; set; }
+        public List<TotalAvailability> TotalAvailabilities { get; set; }
+    }
 
-public class Journey
-{
-    public int RecommendationId { get; set; }
-    public List<Flight> Flights { get; set; }
-    public double ImportTaxAdl { get; set; }
-}
+    public class Journey
+    {
+        public int RecommendationId { get; set; }
+        public List<Flight> Flights { get; set; }
+        public double ImportTaxAdl { get; set; }
+    }
 
-public class Flight
-{
-    public string Number { get; set; }
-    public Airport AirportDeparture { get; set; }
-    public Airport AirportArrival { get; set; }
-    public string DateDeparture { get; set; }
-    public string DateArrival { get; set; }
-    public string CompanyCode { get; set; }
-}
+    public class Flight
+    {
+        public string Number { get; set; }
+        public Airport AirportDeparture { get; set; }
+        public Airport AirportArrival { get; set; }
+        public string DateDeparture { get; set; }
+        public string DateArrival { get; set; }
+        public string CompanyCode { get; set; }
+    }
 
-public class Airport
-{
-    public string Code { get; set; }
-}
+    public class Airport
+    {
+        public string Code { get; set; }
+    }
 
-public class TotalAvailability
-{
-    public int RecommendationId { get; set; }
-    public double Total { get; set; }
-}
+    public class TotalAvailability
+    {
+        public int RecommendationId { get; set; }
+        public double Total { get; set; }
+    }
 
-public class FlightCsv
-{
-    public double Price { get; set; }
-    public double Taxes { get; set; }
-    public string OutboundAirportDeparture { get; set; }
-    public string OutboundAirportArrival { get; set; }
-    public string OutboundTimeDeparture { get; set; }
-    public string OutboundTimeArrival { get; set; }
-    public string OutboundFlightNumber { get; set; }
-    public string InboundAirportDeparture { get; set; }
-    public string InboundAirportArrival { get; set; }
-    public string InboundTimeDeparture { get; set; }
-    public string InboundTimeArrival { get; set; }
-    public string InboundFlightNumber { get; set; }
+    public class FlightCsvTwoWays
+    {
+        public double Price { get; set; }
+        public double Taxes { get; set; }
+        public string Outbound1AirportDeparture { get; set; }
+        public string Outbound1AirportArrival { get; set; }
+        public string Outbound1TimeDeparture { get; set; }
+        public string Outbound1TimeArrival { get; set; }
+        public string Outbound1FlightNumber { get; set; }
+        public string Inbound1AirportDeparture { get; set; }
+        public string Inbound1AirportArrival { get; set; }
+        public string Inbound1TimeDeparture { get; set; }
+        public string Inbound1TimeArrival { get; set; }
+        public string Inbound1FlightNumber { get; set; }
+    }
+
+    public class FlightCsv
+    {
+        public double Price { get; set; }
+        public double Taxes { get; set; }
+        public string Outbound1AirportDeparture { get; set; }
+        public string Outbound1AirportArrival { get; set; }
+        public string Outbound1TimeDeparture { get; set; }
+        public string Outbound1TimeArrival { get; set; }
+        public string Outbound1FlightNumber { get; set; }
+        public string Outbound2AirportDeparture { get; set; }
+        public string Outbound2AirportArrival { get; set; }
+        public string Outbound2TimeDeparture { get; set; }
+        public string Outbound2TimeArrival { get; set; }
+        public string Outbound2FlightNumber { get; set; }
+        public string Inbound1AirportDeparture { get; set; }
+        public string Inbound1AirportArrival { get; set; }
+        public string Inbound1TimeDeparture { get; set; }
+        public string Inbound1TimeArrival { get; set; }
+        public string Inbound1FlightNumber { get; set; }
+        public string Inbound2AirportDeparture { get; set; }
+        public string Inbound2AirportArrival { get; set; }
+        public string Inbound2TimeDeparture { get; set; }
+        public string Inbound2TimeArrival { get; set; }
+        public string Inbound2FlightNumber { get; set; }
+    }
 }
--- FlightScraper/Program.cs
+++ FlightScraper/Program.cs
@@ -1,4 +1,5 @@
-﻿using CsvHelper;
+﻿using System.Diagnostics;
+using CsvHelper;
 using Newtonsoft.Json;
 using System.Globalization;
 using FlightScraper.Models;
@@ -9,9 +10,36 @@ namespace FlightScraper
     {
         static async Task Main(string[] args)
         {
-            var flightsData = await FetchFlightData();
-            var flightCsv = MapToCsv(flightsData);
-            ExportToCsv(flightCsv);
+            string url = BuildUrl();
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(url);
+                
+                if (response == "<H1> Departure Date Not Available </H1>")
+                {
+                    Console.WriteLine("Departure date not available. Stopping execution.");
+                    return;
+                }
+            }
+
+            try
+            {
+                var flightsData = await FetchFlightData();
+                if (flightsData != null)
+                {
+                    var flightCsvTwoWays = MapToCsvTwoWays(flightsData);
+                    var flightCsv = MapToCsv(flightsData);
+                    ExportToCsv(flightCsvTwoWays, flightCsv);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to fetch flight data.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unhandled exception: {ex.Message}");
+            }
         }
 
         static async Task<FlightModel> FetchFlightData()
@@ -28,13 +56,13 @@ namespace FlightScraper
         {
             string baseUrl = "http://homeworktask.infare.lt/search.php";
             string from = "MAD";
-            string to = "AUH";
+            string to = "FUE";
             string depart = "2024-08-23";
             string returnDate = "2024-08-29";
             return $"{baseUrl}?from={from}&to={to}&depart={depart}&return={returnDate}";
         }
-        
-        static string BuildFilename(string url)
+
+        static string BuildFilename(string url, string suffix)
         {
             Uri uri = new Uri(url);
             var queryParams = System.Web.HttpUtility.ParseQueryString(uri.Query);
@@ -44,17 +72,16 @@ namespace FlightScraper
             string depart = queryParams["depart"];
             string returnDate = queryParams["return"];
 
-            return $"flights-{from}-{to}-{depart}-{returnDate}.csv";
+            return $"flights-{from}-{to}-{depart}-{returnDate}-{suffix}.csv";
         }
 
-        static List<FlightCsv> MapToCsv(FlightModel flightData)
+        static List<FlightCsvTwoWays> MapToCsvTwoWays(FlightModel flightData)
         {
-            var flightCsv = new List<FlightCsv>();
+            var flightCsvTwoWays = new List<FlightCsvTwoWays>();
             var journeys = flightData.Body.Data.Journeys;
             var totalAvailabilities = flightData.Body.Data.TotalAvailabilities.ToDictionary(ta => ta.RecommendationId, ta => ta.Total);
 
             var groupedJourneys = journeys
-                .Where(j => j.Flights.Count == 1)
                 .GroupBy(j => j.RecommendationId)
                 .Select(g => new
                 {
@@ -66,38 +93,119 @@ namespace FlightScraper
 
             foreach (var group in groupedJourneys)
             {
-                var firstFlight = group.Flights.First();
-                var secondFlight = group.Flights.Last();
-
-                flightCsv.Add(new FlightCsv
+                if (group.Flights.Count == 2)
                 {
-                    Price = group.TotalPrice,
-                    Taxes = group.TotalTaxes,
-                    OutboundAirportDeparture = firstFlight.AirportDeparture.Code,
-                    OutboundAirportArrival = firstFlight.AirportArrival.Code,
-                    OutboundTimeDeparture = firstFlight.DateDeparture,
-                    OutboundTimeArrival = firstFlight.DateArrival,
-                    OutboundFlightNumber = firstFlight.CompanyCode + firstFlight.Number,
-                    InboundAirportDeparture = secondFlight.AirportDeparture.Code,
-                    InboundAirportArrival = secondFlight.AirportArrival.Code,
-                    InboundTimeDeparture = secondFlight.DateDeparture,
-                    InboundTimeArrival = secondFlight.DateArrival,
-                    InboundFlightNumber = secondFlight.CompanyCode + secondFlight.Number
+                    var firstFlight = group.Flights[0];
+                    var secondFlight = group.Flights[1];
+
+                    flightCsvTwoWays.Add(new FlightCsvTwoWays
+                    {
+                        Price = group.TotalPrice,
+                        Taxes = Math.Round(group.TotalTaxes, 2),
+                        Outbound1AirportDeparture = firstFlight.AirportDeparture.Code,
+                        Outbound1AirportArrival = firstFlight.AirportArrival.Code,
+                        Outbound1TimeDeparture = firstFlight.DateDeparture,
+                        Outbound1TimeArrival = firstFlight.DateArrival,
+                        Outbound1FlightNumber = firstFlight.CompanyCode + firstFlight.Number,
+                        Inbound1AirportDeparture = secondFlight.AirportDeparture.Code,
+                        Inbound1AirportArrival = secondFlight.AirportArrival.Code,
+                        Inbound1TimeDeparture = secondFlight.DateDeparture,
+                        Inbound1TimeArrival = secondFlight.DateArrival,
+                        Inbound1FlightNumber = secondFlight.CompanyCode + secondFlight.Number,
+                    });
+                }
+            }
+
+            return flightCsvTwoWays;
+        }
+
+        static List<FlightCsv> MapToCsv(FlightModel flightData)
+        {
+            var flightCsv = new List<FlightCsv>();
+            var journeys = flightData.Body.Data.Journeys;
+            var totalAvailabilities = flightData.Body.Data.TotalAvailabilities.ToDictionary(ta => ta.RecommendationId, ta => ta.Total);
+
+            var groupedJourneys = journeys
+                .GroupBy(j => j.RecommendationId)
+                .Select(g => new
+                {
+                    RecommendationId = g.Key,
+                    Flights = g.SelectMany(j => j.Flights).ToList(),
+                    TotalPrice = totalAvailabilities[g.Key],
+                    TotalTaxes = g.Sum(j => j.ImportTaxAdl)
                 });
+
+            foreach (var group in groupedJourneys)
+            {
+                if (group.Flights.Count == 4)
+                {
+                    var firstOutboundFlight = group.Flights[0];
+                    var secondOutboundFlight = group.Flights[1];
+                    var firstInboundFlight = group.Flights[2];
+                    var secondInboundFlight = group.Flights[3];
+
+                    flightCsv.Add(new FlightCsv
+                    {
+                        Price = group.TotalPrice,
+                        Taxes = Math.Round(group.TotalTaxes, 2),
+                        Outbound1AirportDeparture = firstOutboundFlight.AirportDeparture.Code,
+                        Outbound1AirportArrival = firstOutboundFlight.AirportArrival.Code,
+                        Outbound1TimeDeparture = firstOutboundFlight.DateDeparture,
+                        Outbound1TimeArrival = firstOutboundFlight.DateArrival,
+                        Outbound1FlightNumber = firstOutboundFlight.CompanyCode + firstOutboundFlight.Number,
+                        Outbound2AirportDeparture = secondOutboundFlight.AirportDeparture.Code,
+                        Outbound2AirportArrival = secondOutboundFlight.AirportArrival.Code,
+                        Outbound2TimeDeparture = secondOutboundFlight.DateDeparture,
+                        Outbound2TimeArrival = secondOutboundFlight.DateArrival,
+                        Outbound2FlightNumber = secondOutboundFlight.CompanyCode + secondOutboundFlight.Number,
+                        Inbound1AirportDeparture = firstInboundFlight.AirportDeparture.Code,
+                        Inbound1AirportArrival = firstInboundFlight.AirportArrival.Code,
+                        Inbound1TimeDeparture = firstInboundFlight.DateDeparture,
+                        Inbound1TimeArrival = firstInboundFlight.DateArrival,
+                        Inbound1FlightNumber = firstInboundFlight.CompanyCode + firstInboundFlight.Number,
+                        Inbound2AirportDeparture = secondInboundFlight.AirportDeparture.Code,
+                        Inbound2AirportArrival = secondInboundFlight.AirportArrival.Code,
+                        Inbound2TimeDeparture = secondInboundFlight.DateDeparture,
+                        Inbound2TimeArrival = secondInboundFlight.DateArrival,
+                        Inbound2FlightNumber = secondInboundFlight.CompanyCode + secondInboundFlight.Number
+                    });
+                }
             }
 
             return flightCsv;
         }
 
-        static void ExportToCsv(List<FlightCsv> flightCsv)
+        static void ExportToCsv(List<FlightCsvTwoWays> flightCsvTwoWays, List<FlightCsv> flightCsv)
         {
-            using (var writer = new StreamWriter(BuildFilename(BuildUrl())))  // path to file /FlightScraper/bin/Debug/net8.0/flights-MAD-AUH-2024-08-23-2024-08-29.csv
-            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            string url = BuildUrl();
+
+            if (flightCsvTwoWays.Any())
             {
-                csv.WriteRecords(flightCsv);
+                using (var writer = new StreamWriter(BuildFilename(url, "two-ways")))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(flightCsvTwoWays);
+                }
+                Console.WriteLine("Two-ways CSV file saved successfully.");
+            }
+            else
+            {
+                Console.WriteLine("No two-ways flight data to save.");
             }
 
-            Console.WriteLine("CSV file saved to: flights.csv");
+            if (flightCsv.Any())
+            {
+                using (var writer = new StreamWriter(BuildFilename(url, "multi-legs")))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(flightCsv);
+                }
+                Console.WriteLine("Multi-legs CSV file saved successfully.");
+            }
+            else
+            {
+                Console.WriteLine("No multi-legs flight data to save.");
+            }
         }
     }
 }
